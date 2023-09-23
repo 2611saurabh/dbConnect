d14a2fec7b813462bdac2fe74aa64ffef47b437e
2611saurabh <saurabh.kr026@gmail.com>
2023-09-23 20:52:49 +0530

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        getConnection();
+    }
+
+    public static Connection getConnection(){
+        try{
+            String driver = "com.mysql.cj.jdbc.Driver";
+            String databaseUrl = "jdbc:mysql://localhost:3306/college";
+            String userName = "root";
+            String password = "11907294";
+            Class.forName(driver);
+            Connection conn = DriverManager.getConnection(databaseUrl,userName,password);
+            System.out.println("Connection done database connected");
+            return conn;
+
+        }
+        catch (Exception e){
+            System.out.println("Sone error" + e);
+        }
+        return null;
     }
 }
